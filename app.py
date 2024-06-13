5a311f59a38dfc55564ab53a3d25b885831cab01
last-brain-cell <naadkd@gmail.com>
2024-06-13 16:07:25 +0530

    demo + changes in experiment

---
+++ app.py
@@ -0,0 +1,64 @@
+import time
+
+import streamlit as st
+from split_experiment.main import SplittingTest
+
+
+def response_generator(splitter: str, query: str):
+    test = SplittingTest(splitter)
+    response = test.query_documents(query)
+
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+st.set_page_config(layout="wide", page_title="Chunking Demo")
+st.title("Chunking Strategies Demo")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    if message["role"] == "user":
+        with st.chat_message("user"):
+            st.markdown(message["content"])
+    else:
+        cols = st.columns(2, gap="small")
+
+        with cols[0]:
+            with st.chat_message("assistant"):
+                semantic_container = st.container(border=True)
+                semantic_container.subheader("Semantic")
+                semantic_container.markdown(message["semantic"])
+        with cols[1]:
+            with st.chat_message("assistant"):
+                recursive_container = st.container(border=True)
+                recursive_container.subheader("Recursive")
+                recursive_container.markdown(message["recursive"])
+
+if prompt := st.chat_input("Ask a Query..."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    cols = st.columns(2, gap="small")
+
+    with cols[0]:
+        with st.chat_message("assistant"):
+            semantic_container = st.container(border=True)
+            semantic_container.subheader("Semantic")
+            semantic_response = semantic_container.write_stream(
+                response_generator(splitter="semantic", query=prompt)
+            )
+    with cols[1]:
+        with st.chat_message("assistant"):
+            recursive_container = st.container(border=True)
+            recursive_container.subheader("Recursive")
+            recursive_response = recursive_container.write_stream(
+                response_generator(splitter="recursive", query=prompt)
+            )
+        st.session_state.messages.append({"role": "assistant", "semantic": semantic_response, "recursive": recursive_response})
+
+    st.rerun()
